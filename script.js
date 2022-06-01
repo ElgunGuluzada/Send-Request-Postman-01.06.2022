5d5e71048e5917411086589f7a695cefe281a3a0
Elgun Guluzada <elgunpg@code.edu.az>
2022-06-02 03:39:45 +0400

    refactor: some changes

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 let content = document.getElementById("content")
+let error = document.getElementById("error")
 let city = document.getElementById("city")
 let country = document.getElementById("country")
 let weather = document.getElementById("weather")
-let condition = document.getElementById("condition")
+// let condition = document.getElementById("condition")
 let cBtn = document.getElementById("cBtn")
 let fBtn = document.getElementById("fBtn")
 let input = document.getElementById("input")
-
+let localTime = document.getElementById("localTime")
+// Jquery
 
 $(document).ready(function () {
     $("#searchBtn").click(function () {
         axios.get(`https://api.weatherapi.com/v1/current.json?key=6bc15cfb31414fbda9f95625221905&q=${input.value}`).then(element => {
+            content.style.visibility = "visible"
+            error.classList.add("d-none")
+
             city.innerText = element.data.location.name
             country.innerText = element.data.location.country
             weather.innerText = element.data.current.temp_c
+            localTime.innerText = element.data.location.localtime
+            // element.data.current.condition = JSON.stringify(element.data.current.condition)
+            // condition.innerHTML = element.data.current.condition
 
             $("#cBtn").click(function () {
                 weather.innerText = element.data.current.temp_c
@@ -21,8 +29,10 @@ $(document).ready(function () {
             $("#fBtn").click(function () {
                 weather.innerText = element.data.current.temp_f
             })
-        });
+        }).catch(
+            error.classList.remove("d-none"),
+            content.style.visibility = "hidden",
+        )
     })
-
 })
 
